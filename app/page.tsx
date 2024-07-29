bda29b2d1a117884c41cbbc24aa75572d9dd4254
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-07-29 14:30:46 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-rose-500
+       font-semibold">
+          Hello Live Wave
+      </p>
+
+      <Button className="mt-5">
+        hello' bahut 
+      </Button>
+    </div>
+    
+  );
+}
