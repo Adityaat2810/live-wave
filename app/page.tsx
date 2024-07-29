646a08e1e6b4421f8be292ab4ecd9bbf77c45c8a
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-07-29 22:42:36 +0530

    setup clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,16 @@
-/**
- * sever component(Server-side rendering) by default 
- * ( no interactivity )
- * 
- * server componet has => 
- *  backend behaviour 
- *  database access 
- *  server cache 
- *  streaming 
- *  partial rendeing 
- *  Seo 
- * initial page loss 
- * 
- * 
- * 
- * "use client " => used to make 
- * clinet componet 
- *   ->frontend behaivoiur 
- *   ->use effect 
- *   ->on click 
- * 
- */
 
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500
-       font-semibold">
-          Hello Live Wave
-      </p>
+   <div className="flex flex-col gap-y-4">
+    <h1>Dashboard</h1>
+    <UserButton
+      afterSignOutUrl="/"
+    />
 
-      <Button className="mt-5">
-        hello' bahut 
-      </Button>
-    </div>
+   </div>
     
   );
 }
